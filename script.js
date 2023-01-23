371826952fb893c91b1067473012002db7440a3e
Ellen Houghton <ellen.a.ash@gmail.com>
2023-01-23 19:20:49 +0000

    add further explanation to page title comments

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ let pageTitle = document.title;
 
 // 2 arrow function expressions
 // function called blur called by the event of the user navigating away from the page in the <title> tag in the window object (not the document object hence window. rather than document.)
+// The function causes the title of the document in the tab to change when the user navigates away from the page.
 window.addEventListener("blur", () => {
     document.title = "Lorem Ipsum";
 });
